f846bbcc039d7d0587b7b19bab29b5a25edc73af
iroczkibela <iroczkib@kkszki.hu>
2024-10-09 16:59:16 +0200

    2 fájl létrehozása

---
+++ WCF_elso_server/Controllers/ZeneszamController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCF_elso_server.Controllers
+{
+    public class ZeneszamController
+    {
+    }
+}
